4a0fb2266ff2867b30fbf73e076d945994a54c3a
FrancoB4 <francobonfigliovazquez@gmail.com>
2025-01-25 18:38:53 -0300

    Se agregan filtros de poco oxigeni, game over e interfaz de game over.

--- .godot/mono/temp/obj/Debug/GameJam-2025.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/GameJam-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameJam-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3d0e46943101491c99f0fa01a5acdb0e831d68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3965373cd708073918304df8fc1ea46d8c5078")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameJam-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameJam-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ _Scripts/Interfaz/FiltroPocoOxigeno.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class FiltroPocoOxigeno : ColorRect
+{
+    private float effectStrength = 0f;
+    private Vector4 centerColor = new Vector4();
+    private bool gameOver = false;
+    private bool terminar = false;
+	public override void _Ready() {
+    }
+
+    public override void _Process(double delta) {
+        if (!terminar) {
+            (Material as ShaderMaterial).SetShaderParameter("effect_strength", effectStrength);
+            if (gameOver) {
+                (Material as ShaderMaterial).SetShaderParameter("center_color", centerColor);
+                if (centerColor == new Vector4(0, 0, 0, 1)) {
+                    terminar = true;
+                }
+            }
+        }
+    }
+
+    private void Activar() {
+        Tween tween = GetTree().CreateTween();
+        tween.TweenProperty(this, "effectStrength", 1, 2)
+        .SetTrans(Tween.TransitionType.Linear);
+    }
+
+    private void Desactivar() {
+        Tween tween = GetTree().CreateTween();
+        tween.TweenProperty(this, "effectStrength", 0, 2)
+        .SetTrans(Tween.TransitionType.Linear);
+    }
+
+    private void FiltroGameOver() {
+        gameOver = true;
+        Tween tween = GetTree().CreateTween();
+        tween.TweenProperty(this, "centerColor", new Vector4(0, 0, 0, 1), 2)
+        .SetTrans(Tween.TransitionType.Linear);
+    }
+}
