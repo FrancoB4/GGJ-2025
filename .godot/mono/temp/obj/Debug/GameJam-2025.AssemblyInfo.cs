ec3965373cd708073918304df8fc1ea46d8c5078
FrancoB4 <francobonfigliovazquez@gmail.com>
2025-01-25 14:33:34 -0300

    Se elimina caustics

--- .godot/mono/temp/obj/Debug/GameJam-2025.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/GameJam-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameJam-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c48bbbe2098fd1fb0f4cc500e7d305e8a85f1f58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3d0e46943101491c99f0fa01a5acdb0e831d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameJam-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameJam-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Godot-RealTimeCaustics-4.3-/CONTRIBUTORS.md
+++
@@ -1,6 +0,0 @@
-# Project founder
-	Patrick Exner @patrick_exe
-
-## Contributors
-	Nekoto
-	tetrapod00
